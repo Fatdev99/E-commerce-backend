ecb7c535ea926a359a135e5068823085ce87828d
Lê Huy Hoàng <hoang.lh20173912@gmail.com>
2024-09-23 15:42:28 +0700

    env, database setup

---
+++ antiantiApe/src/main/java/com/fadev/antiantiApe/AntiantiApeApplication.java
@@ -0,0 +1,13 @@
+package com.fadev.antiantiApe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiantiApeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiantiApeApplication.class, args);
+	}
+
+}
---
+++ antiantiApe/src/test/java/com/fadev/antiantiApe/AntiantiApeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fadev.antiantiApe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiantiApeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
